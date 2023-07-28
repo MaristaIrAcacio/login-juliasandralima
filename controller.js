53280b857ff93e0171baedf2353f797761a5e30a
julia de lima <lima.machado@alunoescolassociais.org.br>
2023-07-28 10:20:43 -0300

    terminamos de fazer a aula dois

--- controller.js
+++ controller.js
@@ -6,15 +6,26 @@ function acessar() {
         alert("favor preencha os campos para acessar o site");
     } else {
         window.location.href = "aula2.html";
-        console.log(window.location.href);
+
     }
+}
     //FUNÇÃO PARA ARMAZENAR NOMES EM ARRAY 
-    var dadoslista = [] ; 
-    function salvarUser(){
+    var dadoslista = [];
+    function salvarUser() {
         let nomeUser = document.getElementById("nomeUser").value;
-        
-        if(nomeUser){
+
+        if (nomeUser) {
             dadoslista.push(nomeUser);
+            criarLista();
+            
         }
-}
-}
+    }
+    // função para criar lista de usário
+    function criarLista(){
+        let tabela = document.getElementById('tabela').innerHTML =   "<tr><th>Nome Usuario</th><th>Ações</th></tr>";
+
+        for(let i = 0;i<= (dadoslista.length - 1);i++){
+            tabela += "<tr><td>" + dadoslista[i] + "</td><td><button class='btn btn-success' onclick=''>Editar</button><button class='btn btn-danger' onclick=''>Excluir</button></td></tr>";
+            document.getElementById('tabela').innerHTML=tabela;
+        }
+    }
